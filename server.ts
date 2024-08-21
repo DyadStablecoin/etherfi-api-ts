9026a8d4478fab0d7e0307f7c3340dfa29cefa6d
Coffee☕️ <coffee@coffeebot.dev>
2024-08-21 09:28:32 -0400

    refactor functions

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request } from "express";
 import { parseAbi, createPublicClient, http, parseEther, formatEther } from "viem";
 import { mainnet } from "viem/chains";
 require("dotenv").config();
@@ -22,52 +22,75 @@ const dnftAbi = parseAbi([
 ]);
 const vaultAbi = parseAbi(["function id2asset(uint256 id) view returns(uint256)"]);
 
-app.get("/", async (req, res) => {
-  try {
-    const totalSupply = await client.readContract({
+async function getBlockNumberOrDefault(req: Request): Promise<bigint> {
+  const blockNumberQuery = req.query["blockNumber"];
+  if (blockNumberQuery && (typeof blockNumberQuery !== "string" || isNaN(parseInt(blockNumberQuery)))) {
+    throw new Error("Invalid block number");
+  }
+  if (!blockNumberQuery) {
+    return await client.getBlockNumber();
+  }
+  return BigInt(blockNumberQuery);
+}
+
+async function getBalancesForAllNotes(blockNumber: bigint): Promise<Record<string, bigint>> {
+  const totalSupply = await client.readContract({
+    address: dnftAddress,
+    abi: dnftAbi,
+    functionName: "totalSupply",
+    blockNumber
+  });
+
+  const ids = Array.from(new Array(Number(totalSupply))).map((_, index) => index);
+
+  const ownersRead = client.multicall({
+    contracts: ids.map((id) => ({
       address: dnftAddress,
       abi: dnftAbi,
-      functionName: "totalSupply",
-    });
+      functionName: "ownerOf",
+      args: [BigInt(id)],
+    })),
+    allowFailure: false,
+    blockNumber
+  }) as unknown as Promise<string[]>;
 
-    const ids = Array.from(new Array(Number(totalSupply))).map((_, index) => index);
+  const balancesRead = client.multicall({
+    contracts: ids.map((id) => ({
+      address: vaultAddress,
+      abi: vaultAbi,
+      functionName: "id2asset",
+      args: [BigInt(id)],
+    })),
+    allowFailure: false,
+    blockNumber
+  });
 
-    const ownersRead = client.multicall({
-      contracts: ids.map((id) => ({
-        address: dnftAddress,
-        abi: dnftAbi,
-        functionName: "ownerOf",
-        args: [BigInt(id)],
-      })),
-      allowFailure: false,
-    }) as unknown as Promise<string[]>;
+  const results = await Promise.all([ownersRead, balancesRead]);
 
-    const balancesRead = client.multicall({
-      contracts: ids.map((id) => ({
-        address: vaultAddress,
-        abi: vaultAbi,
-        functionName: "id2asset",
-        args: [BigInt(id)],
-      })),
-      allowFailure: false,
-    });
+  const balances: Record<string, bigint> = {};
 
-    const results = await Promise.all([ownersRead, balancesRead]);
-
-    const balances: Record<string, bigint> = {};
-
-    for (let i = 0; i < ids.length; i++) {
-      const owner = results[0][i];
-      const balance = results[1][i];
-      if (balance > 0n) {
-        if (balances[owner] === undefined) {
-          balances[owner] = 0n;
-        }
-        balances[owner] += balance;
+  for (let i = 0; i < ids.length; i++) {
+    const owner = results[0][i];
+    const balance = results[1][i];
+    if (balance > 0n) {
+      if (balances[owner] === undefined) {
+        balances[owner] = 0n;
       }
+      balances[owner] += balance;
     }
+  }
 
-    const formattedBalances = Object.entries(balances).map(([owner, balance]) => ({ 
+  return balances;
+}
+
+app.get("/", async (req, res) => {
+  try {
+
+    const blockNumber = await getBlockNumberOrDefault(req);
+
+    const balances = await getBalancesForAllNotes(blockNumber);
+
+    const formattedBalances = Object.entries(balances).map(([owner, balance]) => ({
       address: owner,
       effective_balance: parseFloat(formatEther(balance))
     }));
